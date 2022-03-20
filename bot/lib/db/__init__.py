548f2ffde2923d3ff24260e3a2271f9c25f8d899
AroshHeenkenda <97724307+AroshHeenkenda@users.noreply.github.com>
2022-03-21 10:51:22 +1100

    Data and lib files for the bot

---
+++ bot/lib/db/__init__.py
@@ -0,0 +1,11 @@
+"""
+Author: Arosh Heenkenda
+Created: 12/01/2022
+Edited: 21/03/2022
+Purpose: Initialisation file for database, which isn't currently being used.
+"""
+
+
+from . import db
+
+db.build()
